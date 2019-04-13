b571cfd8b7a09f24f8a2e06517d8032f0079cad4
adrianagraybill <adriana.graybill@gmail.com>
2019-04-13 01:10:48 -0700

    images are rendering

--- js/app.js
+++ js/app.js
@@ -1,98 +1,63 @@
 'use strict';
-// debugger;
-
-let animals = [];
-
-
 
 function Animal(animal) {
   for (let key in animal) {
-    console.log('key', key);
     this[key] = animal[key];
   }
 }
 
-
-//   if (!(Animal.noDuplicate.indexOf(animal.keyword) + 1)) {
-//     Animal.noDuplicate.push(animal.keyword);
-//   }
-
-// }
-
-
-// Animal.noDuplicate = [];
-// console.log('nodup', Animal.noDuplicate);
-// Animal.allAnimals = [];
-// console.log('animal.allaniml', Animal.allAnimals);
+Animal.allAnimals = [];
+Animal.keywords = [];
 
 Animal.prototype.toHtml = function () {
   let $template = $('#animal-template').html();
-  console.log('src', $template);
   let compiledTemplate = Handlebars.compile($template);
-  console.log(compiledTemplate(this));
   return compiledTemplate(this);
+};
+
+Animal.readJson = () => {
+  $.get('data/page-1.json', 'json')
+    .then(data => {
+      data.forEach(item => {
+        Animal.allAnimals.push(new Animal(item));
+      });
+    })
+    .then(populateKeywords)
+    .then(sortKeywords)
+    .then(Animal.loadAnimals)
+    .then(Animal.loadKeyword);
+};
+
+function populateKeywords() {
+  Animal.allAnimals.forEach(animal => {
+    if (!Animal.keywords.includes(animal.keyword)) {
+      Animal.keywords.push(animal.keyword)
+    }
+  })
 }
 
-
-//   $('main').append('<div class="dupe"></div>');
-//   let animalDupe = $('div[class="dupe"]');
-
-//   let animalHtml = $('#photo-template').html();
-
-//   animalDupe.html(animalHtml);
-
-//   animalDupe.find('h2').text(this.title);
-//   animalDupe.find('img').attr('src', this.image_url);
-//   animalDupe.find('#description').text(this.description);
-//   animalDupe.find('#keyword').text(this.keyword);
-//   animalDupe.find('#horns').text(this.horns);
-//   animalDupe.removeClass('dupe');
-//   animalDupe.attr('class', this.keyword);
-// };
-
-// Animal.readJson = () => {
-//   $.get('data/page-1.json', 'json')
-//     .then(data => {
-//       data.forEach(item => {
-//         Animal.allAnimals.push(new Animal(item));
-//       });
-//     })
-//     .then(Animal.loadAnimals)
-//     .then(Animal.loadKeyword);
-// };
-
-
-$.get('data/page-1.json', 'json').forEach(animalObject => {
-  animals.push(new Animal(animalObject));
-});
-
-animals.forEach(newAnimalObject => {
-  $('#animals').append(ourNewNeighborhoodObject.toHtml());
- 
-});
-
-
+function sortKeywords() {
+  Animal.keywords.sort()
+}
 
 Animal.loadAnimals = () => {
-  console.log('load animals');
-  Animal.allAnimals.forEach(animal => animal.render());
+  Animal.allAnimals.forEach(animal => {
+    $('#animals').append(animal.toHtml())
+  });
 };
 
 $(() => Animal.readJson());
 
 Animal.loadKeyword = () => {
-  // debugger;
-  Animal.noDuplicate.forEach((keyword) => {
-    $('select').append(`<option value="${keyword}">${keyword}</option>`);
-  });
-
+  Animal.keywords.forEach((keyword) => {
+    $('#filter').append(`<option value="${keyword}">${keyword}</option>`);
+  })
 };
 
-
-$('select').on('change', function () {
-  let $selection = $(this).val();
-  $('div').hide();
-  $(`div[class="${$selection}"]`).show();
-});
-
-
+Animal.filterKeyword = () => {
+  $('#filter').on('change', function () {
+    let $selection = $(this).val();
+    $('div').hide();
+    $(`div[class="${$selection}"]`).show();
+  });
+}
